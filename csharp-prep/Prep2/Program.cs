50de840e7e16c94799e2585dbc7be48d73344ad9
Nathaniel M <54046320+TheoryInfinity@users.noreply.github.com>
2025-01-13 22:29:42 -0700

    added code to promt the user for a grade percentage and then return their grade letter and passing status.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string gradeInput = Console.ReadLine();
+        int grade = int.Parse(gradeInput);
+        if (grade >= 90)
+        {
+            Console.WriteLine("You have an A");
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            Console.WriteLine("You have a B");
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            Console.WriteLine("You have a C");
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            Console.WriteLine("You have a D");
+        }
+        else
+        {
+            Console.WriteLine("You have an F");
+        }
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congradulations! You are passing!");
+        }
     }
 }
