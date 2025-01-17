0a9ae22ef7d906301e56cabb463196a4000625e9
Nathaniel M <54046320+TheoryInfinity@users.noreply.github.com>
2025-01-16 20:52:44 -0700

    added code to practice fucntion creation

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string username = PromptUserName();
+        int favoriteNumber = PromptUserNumbers();
+        int numberSquared = SquareNumber(favoriteNumber);
+        DisplayResults(username, numberSquared);
+
+    }
+
+    static void DisplayWelcome() {
+    Console.WriteLine("Welcome!");
+    }
+
+    static string PromptUserName() {
+        Console.Write("Please enter your username:");
+        string Username = Console.ReadLine();
+        return Username;
+    }
+
+    static int PromptUserNumbers() {
+        Console.Write("Please enter your favorite number:");
+        string favoriteInput = Console.ReadLine();
+        int favoriteNumber = Convert.ToInt32(favoriteInput);
+        return favoriteNumber;
+    }
+
+    static int SquareNumber(int number) {
+        int numberSquared = number * number;
+        return numberSquared;
+    }
+
+    static void DisplayResults(string username, int number) {
+            Console.WriteLine($"Your username is {username}.");
+            Console.WriteLine($"Your favorite number squared is {number}.");
     }
 }
