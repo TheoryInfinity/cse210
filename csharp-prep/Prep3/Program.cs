549543f18d64eed63df0fb2d293fe9afa8d80bc5
Nathaniel M <54046320+TheoryInfinity@users.noreply.github.com>
2025-01-16 18:07:03 -0700

    added code to create a magic number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 10);
+
+        bool done = false;
+
+        int guess = 0;
+
+        while (done != true) {
+            Console.WriteLine($"What is your guess?");
+            string guessInput = Console.ReadLine();
+            guess = Convert.ToInt32(guessInput);
+            if (guess > magicNumber) {
+                Console.WriteLine("Guess Lower");
+            } else if (guess < magicNumber )
+            {
+                Console.WriteLine("Guess Higher");
+            } else {
+                Console.WriteLine("Correct! You Guessed it!");
+                done = true;
+            }
+        }
+
     }
 }
